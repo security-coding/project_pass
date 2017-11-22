8bf2cb30c5f028fc293e724657da34aa7cad0bef
ILSIKCHOI <33270088+ILSIKCHOI@users.noreply.github.com>
2017-11-22 14:53:44 +0900

    sfd
    sdfsdf

--- src/main/java/com/pknu/pass/play/controller/PlayController.java
+++ src/main/java/com/pknu/pass/play/controller/PlayController.java
@@ -1,26 +1,13 @@
 package com.pknu.pass.play.controller;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.pknu.pass.admin.service.AdminService;
 import com.pknu.pass.play.dto.MainDto;
 import com.pknu.pass.play.service.PlayService;
 
@@ -51,12 +38,14 @@ public class PlayController {
 //	}
 	@ResponseBody
 	@RequestMapping(value = "/change")
-	public List<MainDto> getChange(String type) throws Exception {
+	public ArrayList<MainDto> getChange(String type) throws Exception {
 		
-		List<MainDto> fileNames = new ArrayList<>(); 
+		ArrayList<MainDto> fileNames = new ArrayList<>(); 
 		
 		fileNames = playService.getChange(type);
-		
+//		for(int i=0;i<fileNames.size();i++){
+//			System.out.println(fileNames.get(i).toString());
+//		}
 		return fileNames;
 
 	}
